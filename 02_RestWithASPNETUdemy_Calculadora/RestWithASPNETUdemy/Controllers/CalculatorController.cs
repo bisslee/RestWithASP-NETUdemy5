28e0dcf8144a5c3a05fcd4e97a5e3e5434299503
Ivana Batista dos Santos <bisslee@gmail.com>
2020-11-25 03:52:47 -0300

    implementando a calculadora

---
+++ 02_RestWithASPNETUdemy_Calculadora/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if (IsNumeric(firstnumber)&& IsNumeric(secondnumber))
+            {
+                var sum = ConvertToDecimal(firstnumber) + ConvertToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber
+                , System.Globalization.NumberStyles.Any
+                , System.Globalization.NumberFormatInfo.InvariantInfo
+                , out number);
+            return isNumber;
+        }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalvalue;
+
+            if (decimal.TryParse(strNumber, out decimalvalue))
+            {
+                return decimalvalue;
+            }
+
+            return 0;
+        }
+
+
+    }
+}
