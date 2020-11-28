45953e2ce4869ea162302b10dcfa50b1f712ba09
Ivana Batista dos Santos <bisslee@gmail.com>
2020-11-28 04:30:38 -0300

    iniciando o VO

---
+++ 13_RestWithASPNETUdemy_VO/RestWithASPNETUdemy/Business/Implementations/BookBusinessImplemantation.cs
@@ -0,0 +1,49 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class BookBusinessImplemantation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookBusinessImplemantation(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Book Create(Book book)
+        {
+
+            return _repository.Create(book);
+
+        }
+
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+           _repository.Delete(id);
+        }
+
+    }
+}
