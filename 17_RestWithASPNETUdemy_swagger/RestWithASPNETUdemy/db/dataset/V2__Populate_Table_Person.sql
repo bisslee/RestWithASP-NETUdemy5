ff6be8eefb3f406b098a5a0ab44a335528509d94
Ivana Batista dos Santos <bisslee@gmail.com>
2020-11-29 04:56:47 -0300

    swagger

---
+++ 17_RestWithASPNETUdemy_swagger/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+﻿
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'sampa', 'Jeremias', 'dog', 'Lee'),
+	(2, 'Sampa', 'ivana', 'Female', 'santos'),
+	(3, 'Sampa', 'Biss', 'Female', 'Lee III'),
+	(4, 'Sampa', 'Alfredo', 'dog', 'Lee'),
+	(6, 'Sampa', 'Peter', 'Note', 'Lee');
---
+++ 17_RestWithASPNETUdemy_swagger/RestWithASPNETUdemy/db/dataset/V4__Populate_Table_Book.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `book` (`id`, `title`, `author`, `isbn`, `edition`, `price`,`launch_date`  ) VALUES
+	(1, 'HP e o eniga', 'JK R', 'asdasda2423', '23a.', 75, '2004-10-21'),
+	(2, 'O Homem que calculava', 'Malba Tahan', '345sdfsd234', '123a.', 62, '1973-08-23')	;
+
